39f1c8f519988799b02120540cb1b7a183af4c1b
Woody Evins <woody.evins@gmail.com>
2019-08-25 14:31:13 -0500

    add readout component

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
-export { default as Generator } from './Generator'
 export { default as Field } from './Field'
 export { default as Form } from './Form'
+export { default as Generator } from './Generator'
+export { default as Readout } from './Readout'
